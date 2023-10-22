77c38a9078bd18b9cea5d4ed2fe1d7d00d34daff
Septi Lutfiana <septilutfiana79@gmail.com>
2023-10-22 13:21:52 +0700

    Week08 Rute dan Navigation

---
+++ week08/week08/lib/models/item.dart
@@ -0,0 +1,12 @@
+class Item {
+  String name, gambar;
+  int price, stok;
+  double rating;
+
+  Item(
+      {required this.name,
+      required this.price,
+      required this.gambar,
+      required this.stok,
+      required this.rating});
+}
